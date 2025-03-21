6b0dbc1338045fa00f9374ef2d2f4023dd60c445
venkata kailash tanniru <69431876+Kailash-tanniru@users.noreply.github.com>
2025-03-22 01:55:06 +0530

    Update page.tsx

--- app/auth/reset-password-otp/page.tsx
+++ app/auth/reset-password-otp/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect,Suspense } from 'react';
 import Link from 'next/link';
 import Footer from '@/components/shared/Footer';
 import { useAuthStore } from '@/lib/stores/authStore';
